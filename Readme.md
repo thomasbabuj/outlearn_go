28cd0b463a3b72a1d4703eac87d00c8baab59596
Thomas Babu <thomas@interuptive.com>
2016-04-01 22:20:19 +0800

    Inital commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Building Web services in Go - Curated articles by Mat Ryer in Outlearn.com
+
+1) Writing Web applications
+     - By reading the article from golang.org (https://golang.org/doc/articles/wiki/)
---
+++ gowiki/wiki.go
@@ -0,0 +1,48 @@
+package main
+
+import "io/ioutil"
+
+/*
+ Defining the data structures for our wiki.Page struct
+ has two fields title and body. The body element is a byte slice
+ rather then a string becuase that is the type expected by the
+ io libraries.
+*/
+type Page struct {
+	Title string
+	Body  []byte // a byte slice
+}
+
+/*
+Save method is to persist Page data
+This method signature reads - "This is a method named save
+that takes as its receiver p a pointer to Page. It take no parameter and
+returns a value of type error"
+
+WriteFile is a standard library function that writes a byte slice to a file
+
+While writing the file, if anything goes wrong WriteFile will return an error.
+If all goes well, save method will return nil ( zero value for pointers,
+interfaces and some other types.)
+*/
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+/*
+Loadpage function read a given file retruns a pointer to a Page literal
+constructed with the proper title and body values.
+
+function can retrun multiple values, the standard library function
+io.ReadFile returns []byte and error. Here you can collect the body slice
+and ignore the error by using underscore symbol (blank identifier).
+*/
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
